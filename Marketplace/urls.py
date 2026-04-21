7d04e5a35090f4725ab923c529edf061e00fec08
Faris325 <Vlad.getmanov.2014@gmail.com>
2026-04-21 20:13:12 +0300

    correct url

--- Marketplace/urls.py
+++ Marketplace/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),  #Djoser
 
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
